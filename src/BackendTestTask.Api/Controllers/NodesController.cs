4ccb4f3713b02cc40e71663433acbd399d95786a
Oleg Solonko <oleg.solonko@oxagile.com>
2023-05-04 21:35:12 +0200

    Moved migration, added docker compose

---
+++ src/BackendTestTask.Api/Controllers/NodesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackendTestTask.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class NodesController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public NodesController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+    
+    
+}
